a6296433f7556a082e724018885581eb402e63e8
tomaspa-dev <blord.dev@gmail.com>
2024-10-11 13:02:17 -0500

    fixing slides

--- js/script.js
+++ js/script.js
@@ -468,40 +468,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //10 - Sidebar Navigation Lateral
 function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    const iconHorizontal = document.querySelector(".icon-horizontal");
-    const iconVertical = document.querySelector(".icon-vertical");
+    const sidebar = document.getElementById('sidebar');
+    const iconHorizontal = document.querySelector('.icon-horizontal');
+    const iconVertical = document.querySelector('.icon-vertical');
 
-    if (iconHorizontal.style.display !== "none") {
-        // Cambiar a ícono vertical y abrir sidebar
-        iconHorizontal.style.display = "none";
-        iconVertical.style.display = "inline-block";
-        sidebar.style.width = "200px";
+    sidebar.classList.toggle('open'); // Alterna la clase 'open' para abrir o cerrar el sidebar
+
+    if (sidebar.classList.contains('open')) {
+        // Cambiar a ícono vertical y mostrar el sidebar
+        iconHorizontal.style.display = 'none';
+        iconVertical.style.display = 'inline-block';
     } else {
-        // Cambiar a ícono horizontal y cerrar sidebar
+        // Cambiar a ícono horizontal al cerrar el sidebar
         closeSidebar();
     }
 }
 
 function closeSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    const iconHorizontal = document.querySelector(".icon-horizontal");
-    const iconVertical = document.querySelector(".icon-vertical");
+    const sidebar = document.getElementById('sidebar');
+    const iconHorizontal = document.querySelector('.icon-horizontal');
+    const iconVertical = document.querySelector('.icon-vertical');
 
-    // Cerrar el sidebar
-    sidebar.style.width = "0";
+    sidebar.classList.remove('open'); // Remueve la clase 'open' para cerrarla
     
     // Cambiar de vuelta al ícono horizontal
-    iconVertical.style.display = "none";
-    iconHorizontal.style.display = "inline-block";
+    iconVertical.style.display = 'none';
+    iconHorizontal.style.display = 'inline-block';
 }
 
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('open'); // Alterna la clase 'open' para abrir o cerrar
-}
-
-function closeSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.remove('open'); // Remueve la clase 'open' para cerrarla
-}
+//11-
