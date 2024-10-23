70892ef5811b573b7e0a00ae3f800ef444581166
tomaspa-dev <blord.dev@gmail.com>
2024-10-23 08:26:54 -0500

    adding gsap scroll effect

--- js/script.js
+++ js/script.js
@@ -546,8 +546,39 @@ hoverVideos.forEach(video => {
     });
 });
 
-
 // 13 - GSAP SCroll
+// Función para animar los elementos
+function animateFromBottom(element, delay = 0) {
+    gsap.to(element, {
+        y: 0,
+        opacity: 1,
+        duration: 0.3,
+        delay: delay, // Aplica el retraso
+        ease: "power2.out"
+        });
+    }
+
+    // Observador para detectar cuando los elementos están al 50% del viewport
+    const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const element = entry.target;
+            const delay = [...element.parentElement.children].indexOf(element) * 0.3; // Añade delay por elemento
+            animateFromBottom(element, delay); // Anima cada elemento con un retraso
+            observer.unobserve(element); // Deja de observar después de la animación
+        }
+    });
+  }, { threshold: 0.5 }); // 50% en el viewport
+
+  // Aplica el observador a los elementos fuera de la hero-section
+    document.querySelectorAll('.gsap').forEach(element => {
+        observer.observe(element);
+    });
+
+    // Animar los elementos de la hero-section inmediatamente
+    document.querySelectorAll('.hero-section .gsap').forEach((element, index) => {
+    animateFromBottom(element, index * 0.3); // Anima con delay inmediato
+});
 
 
 // //13 - Optimizar carga de videos
