3ff2fd5b2a5d4440f5d33f97386bc62d36a1301a
tomaspa-dev <blord.dev@gmail.com>
2024-10-28 18:35:57 -0500

    example for update blog

--- js/script-blog.js
+++ js/script-blog.js
@@ -14,10 +14,7 @@ gsap.to("#progress-bar", {
 //2 Animar imagen al hacer scroll
 const details = gsap.utils.toArray(".desktopContentSection:not(:first-child)")
 const photos = gsap.utils.toArray(".desktopPhoto:not(:first-child)")
-
-
 gsap.set(photos, {yPercent:101})
-
 const allPhotos = gsap.utils.toArray(".desktopPhoto")
 
 
@@ -25,27 +22,24 @@ const allPhotos = gsap.utils.toArray(".desktopPhoto")
 let mm = gsap.matchMedia();
 
 // add a media query. When it matches, the associated function will run
-mm.add("(min-width: 600px)", () => {
+mm.add("(min-width: 645px)", () => {
 
-  // this setup code only runs when viewport is at least 600px wide
-  console.log("desktop")
-	
-  ScrollTrigger.create({
-	trigger:".gallery",
-	start:"top top",
-	end:"bottom bottom",
-	pin:".right"
+  // this setup code only runs when viewport is at least 600px wide	
+	ScrollTrigger.create({
+		trigger:".gallery",
+		start:"top top",
+		end:"bottom bottom",
+		pin:".right"
 })
 
 //create scrolltrigger for each details section
 //trigger photo animation when headline of each details section 
 //reaches 80% of window height
 details.forEach((detail, index)=> {
-
 	let headline = detail.querySelector("h1")
 	let animation = gsap.timeline()
-	   .to(photos[index], {yPercent:0})
-	   .set(allPhotos[index], {autoAlpha:0})
+	.to(photos[index], {yPercent:0})
+	.set(allPhotos[index], {autoAlpha:0})
 	ScrollTrigger.create({
 		trigger:headline,
 		start:"top 80%",
@@ -55,8 +49,26 @@ details.forEach((detail, index)=> {
 		markers:false
 	})
 })
-	
 });
 
+// 3 - Texto coloreado
+// Asegúrate de que GSAP y ScrollTrigger están correctamente importados
+gsap.registerPlugin(ScrollTrigger);
 
+// Selecciona todas las palabras en el contenedor de texto
+const words = document.querySelectorAll(".word");
+
+// Configura la animación de color para cada palabra y fija el contenedor hasta que el efecto se complete
+gsap.to(words, {
+    color: "#000", // Cambia el color de cada palabra a negro
+    stagger: 0.1, // Retraso progresivo entre palabras para un efecto gradual
+    scrollTrigger: {
+        trigger: "#textSection",
+        start: "top center", // Inicia cuando la parte superior de #textSection llega al top del viewport
+        end: "+=200%", // Extiende el pin para que dure hasta que el efecto esté completo
+        pin: true, // Fija el contenedor de texto durante el efecto de scroll
+        scrub: 1, // Sincroniza la animación con el desplazamiento
+        // markers: true // Activa los marcadores para verificar el efecto
+    }
+});
 
